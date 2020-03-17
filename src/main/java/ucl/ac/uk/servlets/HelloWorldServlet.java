50efb002c4d1039e98bd35c18b1e2e6a7baec6a3
Hongxiang Chen <h.chen.17@ucl.ac.uk>
2020-03-17 16:34:48 +0000

    update with servlet and jsp

--- src/main/java/ucl/ac/uk/servlets/HelloWorldServlet.java
+++ src/main/java/ucl/ac/uk/servlets/HelloWorldServlet.java
@@ -9,7 +9,7 @@ import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-@WebServlet("/helloworldservlet.html")
+@WebServlet("/helloworldservlet.abc")
 public class HelloWorldServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
--- src/main/java/ucl/ac/uk/servlets/HelloWorldServletAndJSP.java
+++ src/main/java/ucl/ac/uk/servlets/HelloWorldServletAndJSP.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/helloworldservletandjsp.html")
+@WebServlet("/helloworldservletandjsp.def")
 public class HelloWorldServletAndJSP extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
